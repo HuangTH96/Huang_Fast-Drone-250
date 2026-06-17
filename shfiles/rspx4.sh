b6f14d19866321df82b07c40fe03714f81238981
PX4-1 <huangtianhao9@gmail.com>
2026-06-17 14:51:57 +0800

    chore: change serial port name for pixhawk

--- shfiles/rspx4.sh
+++ shfiles/rspx4.sh
@@ -1,5 +1,5 @@
-sudo chmod 777 /dev/ttyACM0 & sleep 2;
+sudo chmod 777 /dev/ttyPixhawk & sleep 2;
 roslaunch realsense2_camera rs_camera.launch & sleep 10;
-roslaunch mavros px4.launch & sleep 10;
+roslaunch mavros px4.launch fcu_url:=/dev/ttyPixhawk:921600 & sleep 10;
 roslaunch vins fast_drone_250.launch
 wait;
